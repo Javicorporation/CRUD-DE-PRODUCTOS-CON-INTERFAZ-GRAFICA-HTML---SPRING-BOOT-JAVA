b7c430a68f02e862692d4ef146c84031829703a1
Javicorporation <krchipullajavier@gmail.com>
2024-02-18 22:24:27 -0500

    primer comit

---
+++ src/main/java/com/Proyecto/CrudGestionProductos/CrudGestionProductosApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.CrudGestionProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGestionProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudGestionProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto/CrudGestionProductos/CrudGestionProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.CrudGestionProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGestionProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

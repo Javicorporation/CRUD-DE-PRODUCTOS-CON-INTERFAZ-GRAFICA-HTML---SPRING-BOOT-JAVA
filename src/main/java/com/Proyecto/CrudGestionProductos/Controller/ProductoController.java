cbd62808e22f2c98f8b60d062b703ddf915e75fb
Javicorporation <krchipullajavier@gmail.com>
2024-02-19 23:19:41 -0500

    Actualización, creación de controlador y generación del index HTML

---
+++ src/main/java/com/Proyecto/CrudGestionProductos/Controller/ProductoController.java
@@ -0,0 +1,27 @@
+package com.Proyecto.CrudGestionProductos.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Proyecto.CrudGestionProductos.Service.ProductoService;
+import com.Proyecto.CrudGestionProductos.model.Producto;
+
+@RestController
+@RequestMapping("Gestion")
+public class ProductoController {
+    @Autowired
+    private ProductoService productoService;
+
+    @RequestMapping("/")
+    public String paginaPrincipal(Model model){
+        List<Producto> ListarProductos = productoService.getAll();
+        model.addAttribute("listaProductos", ListarProductos);
+        return "Index";
+    }
+        
+
+}

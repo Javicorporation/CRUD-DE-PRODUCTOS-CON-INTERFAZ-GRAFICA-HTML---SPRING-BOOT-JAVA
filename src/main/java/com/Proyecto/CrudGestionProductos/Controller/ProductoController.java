e7ac0bacf31fcafb9552fe3c11f41f10a268b240
Javicorporation <krchipullajavier@gmail.com>
2024-03-11 23:35:30 -0500

    creación del metodo editar

--- src/main/java/com/Proyecto/CrudGestionProductos/Controller/ProductoController.java
+++ src/main/java/com/Proyecto/CrudGestionProductos/Controller/ProductoController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.Proyecto.CrudGestionProductos.Service.ImProductoService;
 import com.Proyecto.CrudGestionProductos.model.Producto;
@@ -57,5 +59,12 @@ public class ProductoController {
         return "redirect:productos";
     } 
     
+    @RequestMapping("/Nuevo/{id}")
+    public ModelAndView ProductoEditado(@PathVariable("id") Long id){
+        ModelAndView model = new ModelAndView("Producto_editado");
+        Producto producto = imProductoService.findByIdPro(id);
+        model.addObject("producto", producto);
+        return model;
+    }
 
 }

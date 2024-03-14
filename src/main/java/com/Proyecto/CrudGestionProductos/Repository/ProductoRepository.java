f132acf9cd2904bd9f06e038cb7d3337e6ecec32
Javicorporation <krchipullajavier@gmail.com>
2024-03-13 21:39:00 -0500

    creación de lista en el repositorio para el metodo buscar

--- src/main/java/com/Proyecto/CrudGestionProductos/Repository/ProductoRepository.java
+++ src/main/java/com/Proyecto/CrudGestionProductos/Repository/ProductoRepository.java
@@ -1,5 +1,7 @@
 package com.Proyecto.CrudGestionProductos.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.Proyecto.CrudGestionProductos.model.Producto;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
+    List<Producto> buscarProducto(String name, String brand, String supplier);
 
 }
